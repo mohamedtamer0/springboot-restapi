5937907b81bc222d8374440ecb46c29813e1a1e9
mohamedtamer0 <mt4452242@gmail.com>
2023-04-28 19:17:10 +0300

    Path Parameters

--- src/main/java/com/mohamedtamer/springbootrestapi/controller/UsersController.java
+++ src/main/java/com/mohamedtamer/springbootrestapi/controller/UsersController.java
@@ -2,6 +2,7 @@ package com.mohamedtamer.springbootrestapi.controller;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,13 +10,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class UsersController {
 
     @GetMapping("/userget")
-    public String helloGet(){
+    public String helloGet() {
         return "Hello(Get)!";
     }
 
     @PostMapping("/userpost")
-    public String helloPost(){
+    public String helloPost() {
         return "Hello(Post)!";
+    }
+
+
+    // Path Parameters
+    @GetMapping("/users/{id}")
+    public String helloPath(@PathVariable("id") String uuid) {
+        return "Hello!" + uuid;
     }
 
 }
